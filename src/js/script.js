058469ae276060ee12c32a21a2235d801a4ca772
adonesguerreiro <adonesguerreiro@gmail.com>
2021-01-11 09:42:10 -0400

    Realizado requisição da api do covid-19

--- src/js/script.js
+++ src/js/script.js
@@ -2,44 +2,53 @@
 //    stateSearch();
 // }
 
-async function requestApi(url,option) {
-  const response = await axios(url,option);
+async function requestApi(url, option) {
+  const response = await axios(url, option);
 
   return response.data;
-
 }
 
-
-// function requestApiCovid(state) {
-//  const request = requestApi(`https://covid19-brazil-api.now.sh/api/report/v1/brazil/uf/${state}`);
-
-//   return request;
-// }
-
-
 function stateSearch() {
-  
- var url = `https://servicodados.ibge.gov.br/api/v1/localidades/estados`;
+  var url = `https://servicodados.ibge.gov.br/api/v1/localidades/estados`;
 
- const request = requestApi(url);
+  const request = requestApi(url);
 
- request.then((res) => {
-  var optionData = `<option value="" selected>Selecione um estado</option>`
+  request
+    .then((res) => {
+      var optionData = `<option value="" selected>Selecione um estado</option>`;
 
-  optionData+= res.map(states =>{
-    return `<option value=${states.sigla}>${states.nome}</option>`
-  })
+      optionData += res.map((states) => {
+        return `<option value=${states.sigla}>${states.nome}</option>`;
+      });
 
-  var optionState = document.querySelector("#states");
-  optionState.innerHTML = optionData;
+      var optionState = document.querySelector("#states");
+      optionState.innerHTML = optionData;
 
+      optionState.addEventListener("change", function (e) {
+        e.preventDefault();
+        if (optionState != "") {
+          covidStateSearch(optionState.value);
+        }
+      });
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
+}
 
-  
+function covidStateSearch(state) {
+  var url = `https://covid19-brazil-api.now.sh/api/report/v1/brazil/uf/${state}`;
+  const request = requestApi(url);
 
-  console.log(optionData);
- })
-
+  request
+    .then((res) => {
+      console.log("Casos: " + res.cases.toLocaleString("pt-BR"));
+      console.log("Mortes: " + res.deaths.toLocaleString("pt-BR"));
+      console.log("Suspeitos " + res.suspects.toLocaleString("pt-BR"));
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
 }
 
 stateSearch();
-
